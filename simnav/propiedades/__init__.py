0fcf4f52cecbe877aa6fe14444857a0ee19fad62
julioocz <julioocz@gmail.com>
2017-01-01 15:30:29 -0400

    Agregado manejador de parametros

---
+++ simnav/propiedades/__init__.py
@@ -0,0 +1,2 @@
+from . import modelos
+from .propiedades import Parametros
--- simnav/propiedades/modelos.py
+++ simnav/propiedades/modelos.py
@@ -1,11 +1,18 @@
-from .db_handler import db_reflection
+from .base_de_datos import db_reflection
 
 """
-Accesa a la base de datos sqlite para obtener las tablas de propiedades de los componentes
+Accesa a la base de datos sqlite para obtener las tablas de propiedades de los componentes como objetos.
 """
 
-Base, sesion = db_reflection('sqlite:///propiedades.sqlite')
+Base, session = db_reflection('sqlite:///propiedades.sqlite')
 
 # Asignando clases generadas por SQLalchemy
-Componentes = Base.classes.componentes
 CapacidadesCalorificasLiquido = Base.classes.capacidades_calorificas_liquido
+CapacidadesCalorificasGasPolinomial = Base.classes.Capacidades_calorificas_gas_polinomial
+Componentes = Base.classes.componentes
+ConstantesCriticasFactoresAcentricos = Base.classes.Constantes_criticas_y_factores_acentricos
+CpGasHiperbolico = Base.classes.Cp_para_gas_de_forma_hiperbolica
+Antoine = Base.classes.Parametros_ecuacion_de_antoine
+CalorVaporizacionLiquidos = Base.classes.Calor_de_vaporizacion_de_liquidos
+
+
